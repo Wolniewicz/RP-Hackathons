2f86adb23514222fd921718e9b4a300ce35ed898
Jacob Wolniewicz <jacob@wolniewicz.com>
2014-02-17 17:52:24 -0700

    view for both search and view

--- Week 5/birdswitharms/app.py
+++ Week 5/birdswitharms/app.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index ():
     return 'Hello World!'
+
+
+@app.route('/search')
+def resume():
+	return 'Search Page'
+
+
+@app.route('/view')
+def view():
+	return 'View Page'
+
+
 
 if __name__ == '__main__':
     app.run()
