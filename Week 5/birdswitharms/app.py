434cdb332d24d32207ad59eddc4c8691ecc80231
Jacob Wolniewicz <jacob@wolniewicz.com>
2014-02-17 18:11:24 -0700

    search now catches parameters passed in

--- Week 5/birdswitharms/app.py
+++ Week 5/birdswitharms/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,9 +9,18 @@ def index ():
     return 'Hello World!'
 
 
-@app.route('/search')
-def resume():
-	return 'Search Page'
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    temp = request.args.get('n', '')
+    temp2 = request.args.get('query', '')
+    if temp == '1' and temp2 == 'cardinal':
+    	return '1 cardinal'
+    if temp == '1':
+	    return 'one result'
+    else:
+	    return 'nothing passed'
 
 
 @app.route('/view')
